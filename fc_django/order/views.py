407c5cdd232bba2f5ef1104c8684ab950bd8413f
Jaeweon Kim <heyjaekim@gmail.com>
2021-04-09 18:19:18 +0900

    Order Product Form and Page Implemented

--- fc_django/order/views.py
+++ fc_django/order/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views.generic.edit import FormView
+from .forms import RegisterForm
 
 # Create your views here.
+
+class OrderCreate(FormView):
+    form_class = RegisterForm
+    success_url = '/product/'
+
+    def form_invalid(self, form):
+        return redirect('/product/' + str(form.data.get('product')))
+
+    def get_form_kwargs(self, **kwargs):
+        kw = super().get_form_kwargs(**kwargs)
+        kw.update({
+            'request': self.request
+        })
+        return kw
+
