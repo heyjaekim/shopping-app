cfc7c5451262d2bef983a7820a797f72820a1ac6
Jaeweon Kim <heyjaekim@gmail.com>
2021-04-10 22:39:09 +0900

    Refactoring Forms.py to Views.py

--- fc_django/fcuser/views.py
+++ fc_django/fcuser/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import FormView
+from django.contrib.auth.hashers import make_password
 from .forms import RegisterForm, LoginForm
+from .models import Fcuser
 
 # Create your views here.
 
@@ -11,6 +13,16 @@ class RegisterView(FormView):
     template_name = 'register.html'
     form_class = RegisterForm
     success_url = '/'
+
+    def form_valid(self, form):
+        fcuser = Fcuser(
+            email=form.data.get('email'),
+            password=make_password(form.data.get('password')),
+            level='user'
+        )
+        fcuser.save()
+        
+        return super().form_valid(form)
 
 class LoginView(FormView):
     template_name = 'login.html'
--- fc_django/order/forms.py
+++ fc_django/order/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from .models import Order
 from product.models import Product
 from fcuser.models import Fcuser
-from django.db import transaction
 
 
 class RegisterForm(forms.Form):
@@ -25,25 +24,7 @@ class RegisterForm(forms.Form):
         cleaned_data = super().clean()
         quantity = cleaned_data.get('quantity')
         product = cleaned_data.get('product')
-        fcuser = self.request.session.get('user', None)
 
-        if quantity and product and fcuser:
-            with transaction.atomic():
-                prod = Product.objects.get(pk=product)
-                order = Order(
-                    quantity=quantity,
-                    product=Product.objects.get(pk=product),
-                    fcuser=Fcuser.objects.get(email=fcuser)
-                )
-                order.save()
-                prod.stock -= quantity
-                prod.save()
-
-        else:
-            self.product=product
+        if not (quantity and product):
             self.add_error('quantity', '값이 없습니다')
             self.add_error('product', '값이 없습니다')
-
-        # if not (quantity and product and fcuser):
-        #     self.add_error('quantity', '값이 없습니다')
-        #     self.add_error('product', '값이 없습니다')
