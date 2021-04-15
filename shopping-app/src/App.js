fc6b1f7b213608fd33d47157d6976a0960d12756
Jaeweon Kim <heyjaekim@gmail.com>
2021-04-15 23:58:40 +0900

    update shopping-app react folder with redux states

---
+++ shopping-app/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import Subscribers from './components/Subscribers'
+import Display from './components/Display'
+import Views from './components/Views'
+import Comments from './components/Comments'
+import { Provider } from 'react-redux'
+import store from './redux/store'
+
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <Comments />
+        <Subscribers />
+        <Views />
+        <Display />
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
